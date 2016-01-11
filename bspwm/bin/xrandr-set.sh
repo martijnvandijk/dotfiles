73537f89c29b1a0678db654dcb4e9ad79bfc9bd3
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2016-01-11 15:45:35 +0100

    driver-related xrandr changes

--- bspwm/bin/xrandr-set.sh
+++ bspwm/bin/xrandr-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $(hostname)  = "fusion" ]]; then
 	xrandr --output DVI-I-1 --auto --primary
-	xrandr --output HDMI-3 --auto --right-of DVI-I-1 
-	xrandr --output HDMI-4 --auto --left-of DVI-I-1
+	xrandr --output HDMI-0 --auto --right-of DVI-I-1 
+	xrandr --output HDMI-1 --auto --left-of DVI-I-1
 fi
