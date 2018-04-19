0b53fb087963d700bc29ff13dd417f8390b94138
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2018-04-19 10:41:24 +0200

    fixed gpg ssh-auth socket

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,9 +6,9 @@ alias vim=nvim
 
 # configure gpg-agentn SSH auth
 gpg-connect-agent /bye
-if not set -q SSH_AUTH_SOCK
-    {$XDF_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh
-end
+if test -e ~/.gpg-ssh
+    export SSH_AUTH_SOCK={$XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh
+end 
 
 # configure PATH
 set -gx PATH ~/bin $PATH
